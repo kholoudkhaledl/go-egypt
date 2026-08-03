68af98e79b627a616ee22e89bfa82842628b822b
zeinab kotp <zeinabkotp095@gmail.com>
2026-08-03 22:13:26 +0300

    update index.php

---
+++ include/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'include/head.php'; ?>
+</head>
+<!-- Navbar -->
+    <header>
+        <nav>
+
+            <div class="logo">
+                <img src="images/logo.png" alt="Go Egypt Logo">
+                <h2>Go Egypt</h2>
+            </div>
+
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Explore</a></li>
+                <li><a href="#">Landmarks</a></li>
+                <li><a href="#">Virtual Tours</a></li>
+                <li><a href="#">Plan Trip</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+
+            <div class="buttons">
+                <button>Login</button>
+                <button>Sign Up</button>
+            </div>
+
+        </nav>
+    </header>
